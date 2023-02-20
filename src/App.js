e040e5716b8a936cac929a987d98180621da2c94
Jahangir Ashraf Nasirwala <acerdesktop53@gmail.com>
2023-02-21 02:41:03 +0530

    Sections added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 import NavBar from "./navbar";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import AppSection from "./AppSection";
+import CardSection from "./CardSection";
+
+
 const App = () => {
     return(
     <div>
-        <h1 style={{textAlign: "center"}}>Jahangir</h1>
         <NavBar/>
+        <HeroSection/>
+        <AppSection/>
+        <CardSection/>
+        <Footer/>
     </div>
     );
 };
